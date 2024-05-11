96c7d61b3aa5dbd60838477179d48f9488114d52
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-11 15:29:59 +0000

    save

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+   function calculatePi(numIterations) {
+     let pi = 0
+     for (let i = 1; i <= numIterations; i++) {
+       pi += Math.pow(-1, i + 1) / (2 * i - 1)
+     }
+     pi *= 4
+     return pi
+   }
+
+   function calculateAndDisplayPi() {
+     const numIterations = parseInt(
+       document.getElementById("numIterations").value
+     )
+     if (isNaN(numIterations) || numIterations <= 0) {
+       alert("Please enter a positive integer.")
+       return
+     }
+
+     const piValue = calculatePi(numIterations)
+     document.getElementById(
+       "output"
+     ).innerText = `Value of PI after ${numIterations} iterations: ${piValue}`
+   }
