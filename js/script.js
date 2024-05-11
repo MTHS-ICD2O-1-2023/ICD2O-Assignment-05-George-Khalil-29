530738699cd1203cc803290be68a240bfa7f1dd8
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-11 16:28:32 +0000

    save

--- js/script.js
+++ js/script.js
@@ -4,26 +4,24 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-   function calculatePi(numIterations) {
-     let pi = 0
-     for (let i = 1; i <= numIterations; i++) {
-       pi += Math.pow(-1, i + 1) / (2 * i - 1)
-     }
-     pi *= 4
-     return pi
-   }
+function calculatePi(numIterations) {
+  let pi = 0
+  for (let i = 1; i <= numIterations; i++) {
+    pi += Math.pow(-1, i + 1) / (2 * i - 1)
+  }
+  pi *= 4
+  return pi
+}
 
-   function calculateAndDisplayPi() {
-     const numIterations = parseInt(
-       document.getElementById("numIterations").value
-     )
-     if (isNaN(numIterations) || numIterations <= 0) {
-       alert("Please enter a positive integer.")
-       return
-     }
+function calculateAndDisplayPi() {
+  const numIterations = parseInt(document.getElementById("numIterations").value)
+  if (isNaN(numIterations) || numIterations <= 0) {
+    alert("Please enter a positive integer.")
+    return
+  }
 
-     const piValue = calculatePi(numIterations)
-     document.getElementById(
-       "output"
-     ).innerText = `Value of PI after ${numIterations} iterations: ${piValue}`
-   }
+  const piValue = calculatePi(numIterations).toFixed(2)
+  document.getElementById(
+    "output"
+  ).innerHTML = `Value of PI after ${numIterations} iterations: ${piValue}`
+}
